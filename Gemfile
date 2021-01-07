9e352ed1cfc140ace0e9df0de47dfb68f2529706
vysogot <jakub.godawa@gmail.com>
2021-01-07 20:28:06 +0100

    Add simplecov

--- Gemfile
+++ Gemfile
@@ -1,1 +1,4 @@
+source 'https://rubygems.org'
+
 gem 'minitest'
+gem 'simplecov', require: false
--- test.rb
+++ test.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 require_relative 'app'
 require 'minitest/autorun'
 
