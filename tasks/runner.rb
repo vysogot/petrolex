5609c3345f87d02105cdaec550f8ac99fd447c1b
Jakub Godawa <jakub.godawa@gmail.com>
2024-03-16 16:41:46 +0100

    refresh runner

--- tasks/runner.rb
+++ tasks/runner.rb
@@ -2,40 +2,46 @@
 
 require_relative '../app/petrolex'
 
-number_of_cars = 10
-fuel_reserve = 30_000
-tank_range = (35..70)
-level_range = (1...35)
-car_delay_range = (10..100)
-simulation_speed = 1000
-closing_tick = 2000
+SIMULATION_SPEED = 1000
+
+# CARS
+NUMBER_OF_CARS = 10
+TANK_VOLUME_RANGE = (35..70)
+TANK_LEVEL_RANGE = (1...35)
+CAR_DELAY_RANGE = (10..100)
+
+# STATION
+FUEL_RESERVE = 30_000
+CLOSING_TICK = 2_000
 
 Timer.configure do |timer|
-  timer.simulation_speed = simulation_speed
+  timer.simulation_speed = SIMULATION_SPEED
 end
 
-# TODO: problem with starting with a closed station
-station = Station.new(closing_tick:, fuel_reserve:, is_open: true)
-cars = []
-car_threads = []
+cars, car_threads = [], []
+station = Station.new(
+  closing_tick: CLOSING_TICK,
+  fuel_reserve: FUEL_RESERVE
+)
 
-number_of_cars.times do
-  cars << Car.new(tank_volume: rand(tank_range),
-                  tank_level: rand(level_range))
+NUMBER_OF_CARS.times do
+  cars << Car.new(
+    tank_volume: rand(TANK_VOLUME_RANGE),
+    tank_level: rand(TANK_LEVEL_RANGE)
+  )
 end
 
 cars.each do |car|
   car_threads << Thread.new do
-    Timer.instance.pause_until(rand(car_delay_range))
+    Timer.instance.pause_until(rand(CAR_DELAY_RANGE))
     car.queue_up(station)
   end
 end
 
 puts 'Petrolex Station Simulator has started.'
-puts "Simulation speed: x#{simulation_speed}"
+puts "Simulation speed: x#{SIMULATION_SPEED}"
 puts "Fuel reserve: #{station.fuel_reserve}"
-puts "Cars to arrive: #{number_of_cars}"
-puts
+puts "Cars to arrive: #{NUMBER_OF_CARS}\n\n"
 
 Timer.instance.start
 
@@ -43,14 +49,12 @@ station.open
 car_threads.each(&:join)
 
 total_cars_waiting_time = station.waiting_times.sum
-number_of_cars_fueled = number_of_cars - station.queue.size
+number_of_cars_fueled = NUMBER_OF_CARS - station.queue.size
 avg_time = total_cars_waiting_time / number_of_cars_fueled.to_f
 
-puts
 puts 'Results:'
 puts "Cars served: #{station.waiting_times.size}"
 puts "Cars left: #{station.queue.size}"
 puts "Avg car wait: #{avg_time.round(3)} seconds"
-puts "Litres left: #{station.fuel_reserve} litres"
-puts
+puts "Litres left: #{station.fuel_reserve} litres\n"
 puts 'Petrolex Station Simulator has ended.'
